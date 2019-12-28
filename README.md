0c2c451daaec6b29300a3cca19d87c6c7c343f08
Vincent Dauce <exorus.spam@gmail.com>
2019-12-28 16:27:41 +0100

    Add all initial checks

--- README.md
+++ README.md
@@ -14,7 +14,8 @@ Module for testing receiving emails using [MailCare](https://mailcare.io).
 
 ### Configuration
 
-* url *required* - API url of your mailcare server
+* url *optional* - API url of your mailcare server (default: https://mailix.xyz/api)
+* timeoutInSeconds *optional* - Waits up to n seconds for an email to be received (default: 30 seconds)
 
 #### Example
 ```
@@ -24,23 +25,41 @@ modules:
             url: 'https://mailix.xyz/api'
 ```
 
-### Timeout
-
-Waits up to $timeout seconds for an email to be received (30 seconds by default).
-
 ### Criterias
 
  * `inbox`      Filter by inbox (test@example.com).
  * `sender`     Filter by sender (test@example.com).
  * `subject`    Filter by subject (Welcome).
- * `since`      Filter by createdAt (2018-01-19T12:23:27+00:00).
+ * `since`      Filter by createdAt (2018-01-19T12:23:27+00:00 or ISO 8601 durations).
  * `search`     Search by inbox or sender or subject (matching).
  * `unread`     Filter only by unread (true).
  * `favorite`   Filter only by favorite (true).
 
 All criterias can be found in the [API Documentation of MailCare](https://mailcare.docs.apiary.io) except for page and limit.
 
+Examples of `since` with ISO 8601 durations:
+* P1D: one-day duration
+* PT1M: one-minute duration (note the time designator, T, that precedes the time value)
+
 ### Actions
+
+#### seeEmailCount
+
+Checks that the email count equals expected value.
+Waits up to $timeout seconds for the given email to be received.
+
+```php
+$I->seeEmailCount(2, [
+    'inbox' => 'john@example.org',
+    'sender' => 'no-reply@company.com',
+    'subject' => 'Welcome John!',
+    'since' => 'PT2M',
+], 30);
+```
+
+ * `param int`      $expectedCount
+ * `param array`    $criterias
+ * `param int`      $timeoutInSeconds (optional)
 
 #### seeEmail
 
@@ -52,12 +71,12 @@ $I->seeEmail([
     'inbox' => 'john@example.org',
     'sender' => 'no-reply@company.com',
     'subject' => 'Welcome John!',
-    'since' => 'P2M',
+    'since' => 'PT2M',
 ], 30);
 ```
 
  * `param array`    $criterias
- * `param int`      $timeoutInSecond (optional)
+ * `param int`      $timeoutInSeconds (optional)
  
 #### dontSeeEmail
 
@@ -65,12 +84,44 @@ Opposite to seeEmail.
 
 ```php
 $I->dontSeeEmail([
-    'inbox' => 'john@example.org',
-    'sender' => 'no-reply@company.com',
-    'subject' => 'Welcome John!',
-    'since' => 'P2M',
+    'inbox' => 'john@example.org'
+    'since' => 'PT2M',
 ], 30);
 ```
 
  * `param array`    $criterias
- * `param int`      $timeoutInSecond (optional)
+ * `param int`      $timeoutInSeconds (optional)
+
+#### grabLinksInLastEmail
+
+In the last email, grabs all the links
+Waits up to $timeout seconds for the given email to be received.
+
+```php
+$I->grabLinksInLastEmail([
+    'inbox' => 'john@example.org'
+    'since' => 'PT2M',
+], 30);
+```
+
+ * `param array`    $criterias
+ * `param int`      $timeoutInSeconds (optional)
+ * `return array`   ['https://google.fr', 'https://mailcare.io']
+
+
+#### grabTextInLastEmail
+
+In the last email, grabs all the text corresponding to a regex.
+Waits up to $timeout seconds for the given email to be received.
+
+```php
+$I->grabTextInLastEmail($regex, [
+    'inbox' => 'john@example.org'
+    'subject' => 'Your credentials'
+    'since' => 'PT2M',
+], 30);
+```
+
+ * `param array`    $criterias
+ * `param int`      $timeoutInSeconds (optional)
+ * `return array`   matches from preg_match_all
--- src/Codeception/Module/MailCare.php
+++ src/Codeception/Module/MailCare.php
@@ -2,64 +2,218 @@
 namespace Codeception\Module;
 
 use Codeception\Module;
+use DateInterval;
+use DateTime;
+use Exception;
 
 class MailCare extends Module
 {
-
     public $client;
-    protected $baseUrl = 'https://mailix.xyz/api';
-
-
-
-    public function greet($name)
+    protected $url = 'https://mailix.xyz/api';
+    protected $timeoutInSeconds = 30;
+    
+    public function _initialize()
     {
-        $this->debug("Hello {$name}!");
+        if (!empty($this->config['url'])) {
+            $this->url = $this->config['url'];
+        }
+        if (!empty($this->config['timeoutInSeconds'])) {
+            $this->setTimeoutInSeconds($this->config['timeoutInSeconds']);
+        }
     }
 
-    private function getEmails(array $criterias, int $timeoutInSecond): int
+    private function setTimeoutInSeconds(int $timeoutInSeconds): void
     {
-        $end = microtime(true) + $timeoutInSecond;
+        if ($timeoutInSeconds == -1) {
+            $timeoutInSeconds = $this->getTimeoutInSeconds();
+        }
+        $this->timeoutInSeconds = $timeoutInSeconds;
+    }
 
+    private function getTimeoutInSeconds(): int
+    {
+        return $this->timeoutInSeconds;
+    }
+
+    private function sanitizeCriterias(array $criterias): array
+    {
         $criterias = array_merge($criterias, ['limit' => 1, 'page' => 1]);
 
-        $nbEmails = 0;
-        do
-        {
-            $curl = curl_init();
+        if (!empty($criterias['since'])) {
+            try {
+                $duration = new DateInterval($criterias['since']);
+                $now = new DateTime();
+                $now->sub($duration);
+                $criterias['since'] = $now->format(DateTime::ISO8601);
+            } catch (Exception $e) {
+            }
+        }
 
-            curl_setopt_array($curl, [
-                CURLOPT_RETURNTRANSFER => true,
-                CURLOPT_URL => $this->baseUrl . '/emails?' .  http_build_query($criterias),
-                CURLOPT_CUSTOMREQUEST => 'GET', 
-                CURLOPT_USERAGENT => 'codeception-module-mailcare'
-            ]);
+        return $criterias;
+    }
 
-            $resp = curl_exec($curl);
-            $info = curl_getinfo($curl);
+    private function getJson(string $path, $data = []): array
+    {
+        $curl = curl_init();
 
-            $body = json_decode($resp, true);
+        if (empty($data)) {
+            $curlUrl = $this->url . $path;
+        } else {
+            $curlUrl = $this->url . $path . '?' .  http_build_query($data);
+        }
 
-            if ($info['http_code'] == 200 && $body['meta']['total'] >=1) {
-                $nbEmails = $body['meta']['total'];
+        curl_setopt_array($curl, [
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_URL => $curlUrl,
+            CURLOPT_CUSTOMREQUEST => 'GET',
+            CURLOPT_USERAGENT => 'codeception-module-mailcare'
+        ]);
+        
+
+        $resp = curl_exec($curl);
+        $info = curl_getinfo($curl);
+        curl_close($curl);
+
+        if ($info['http_code'] == 200) {
+            return json_decode($resp, true);
+        }
+
+        return [];
+    }
+
+    private function getBody(string $path, $data = []): string
+    {
+        $curl = curl_init();
+
+        if (empty($data)) {
+            $curlUrl = $this->url . $path;
+        } else {
+            $curlUrl = $this->url . $path . '?' .  http_build_query($data);
+        }
+
+        curl_setopt_array($curl, [
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_URL => $curlUrl,
+            CURLOPT_CUSTOMREQUEST => 'GET',
+            CURLOPT_USERAGENT => 'codeception-module-mailcare',
+            CURLOPT_HTTPHEADER => ['Accept: text/html,text/plain,message/rfc2822'],
+        ]);
+        
+
+        $resp = curl_exec($curl);
+        $info = curl_getinfo($curl);
+        curl_close($curl);
+
+        if ($info['http_code'] == 200) {
+            return $resp;
+        }
+
+        return '';
+    }
+
+    private function getLastEmailId(array $criterias): string
+    {
+        $end = microtime(true) + $this->getTimeoutInSeconds();
+
+        $criterias = $this->sanitizeCriterias($criterias);
+
+        $lastId = '';
+        do {
+            $body = $this->getJson('/emails', $criterias);
+
+            if (!empty($body['data'][0]['id'])) {
+                $lastId = $body['data'][0]['id'];
                 break;
             }
 
             usleep(250000);
         } while ($end > microtime(true));
 
-        curl_close($curl);
-        return $nbEmails;
+        return $lastId;
     }
 
-    public function seeEmail(array $criterias, int $timeoutInSecond = 30): void
+    private function getEmailCount(array $criterias): int
     {
-        $nbEmails = $this->getEmails($criterias, $timeoutInSecond);
-        $this->assertGreaterThanOrEqual(1, $nbEmails, "Failed asserting that at least one email has been found.");
+        $end = microtime(true) + $this->getTimeoutInSeconds();
+
+        $criterias = $this->sanitizeCriterias($criterias);
+
+        $count = 0;
+        do {
+            $body = $this->getJson('/emails', $criterias);
+
+            if (!empty($body['meta']['total'])) {
+                $count = $body['meta']['total'];
+                break;
+            }
+
+            usleep(250000);
+        } while ($end > microtime(true));
+
+        return $count;
     }
 
-    public function dontSeeEmail(array $criterias, int $timeoutInSecond = 30): void
+    private function getEmailBody(string $id): string
     {
-        $nbEmails = $this->getEmails($criterias, $timeoutInSecond);
-        $this->assertEquals(0, $nbEmails, "Failed asserting that {$nbEmails} email(s) matches expected 0 email.");
+        $end = microtime(true) + $this->getTimeoutInSeconds();
+
+        $body = '';
+        do {
+            $body = $this->getBody('/emails/'.$id);
+
+            if (!empty($body)) {
+                var_dump($body);
+                $body = $body;
+                break;
+            }
+
+            usleep(250000);
+        } while ($end > microtime(true));
+
+        return $body;
+    }
+
+    public function seeEmailCount(int $expectedCount, array $criterias, int $timeoutInSeconds = -1): void
+    {
+        $this->setTimeoutInSeconds($timeoutInSeconds);
+        $actualCount = $this->getEmailCount($criterias);
+        $this->assertEquals($expectedCount, $actualCount, "Failed asserting that {$actualCount} emails matches expected {$expectedCount}.");
+    }
+
+    public function seeEmail(array $criterias, int $timeoutInSeconds = -1): string
+    {
+        $this->setTimeoutInSeconds($timeoutInSeconds);
+        $lastId = $this->getLastEmailId($criterias);
+        $this->assertNotEmpty($lastId, "Failed asserting that at least one email has been found.");
+        return $lastId;
+    }
+
+    public function dontSeeEmail(array $criterias, int $timeoutInSeconds = -1): void
+    {
+        $this->setTimeoutInSeconds($timeoutInSeconds);
+        $lastId = $this->getLastEmailId($criterias);
+        $this->assertEmpty($lastId, "Failed asserting that no email has been found.");
+    }
+
+    public function grabLinksInLastEmail(array $criterias, int $timeoutInSeconds = -1): array
+    {
+        $lastId = $this->seeEmail($criterias, $timeoutInSeconds);
+
+        $body = $this->getEmailBody($lastId);
+
+        preg_match_all('#\bhttp?://[^,\s()<>]+(?:\([\w\d]+\)|([^,[:punct:]\s]|/))#', $body, $out);
+
+        return $out[0];
+    }
+
+    public function grabTextInLastEmail(string $regex, array $criterias, int $timeoutInSeconds = -1): array
+    {
+        $lastId = $this->seeEmail($criterias, $timeoutInSeconds);
+
+        $body = $this->getEmailBody($lastId);
+
+        preg_match_all($regex, $body, $out);
+
+        return $out;
     }
 }
