cc70e2c1c356091730e84f925a57906bac11c0b2
Vincent Dauce <exorus.spam@gmail.com>
2019-12-28 19:53:05 +0100

    Add tests

---
+++ tests/MailCareTest.php
@@ -0,0 +1,14 @@
+<?php
+declare(strict_types=1);
+
+use PHPUnit\Framework\TestCase;
+
+final class MailCareTest extends TestCase
+{
+    public function testCanBeCreatedFromValidEmailAddress(): void
+    {
+        $this->assertTrue(true);
+    }
+}
+
+
